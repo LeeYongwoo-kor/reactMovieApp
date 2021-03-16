63e6826bee3868846b5396d01be4a46ac61e7bed
LeeYongwoo-kor <newlyo1205@naver.com>
2021-03-16 23:57:13 +0900

    add html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import axios from "axios";
-import Movie from "./Movie";
+
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h3 class="movie__year">{year}</h3>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -21,22 +33,25 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => {
-              console.log(movie);
-              return (
-                <Movie
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.poster}
-                />
-              );
-            })}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.poster}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
